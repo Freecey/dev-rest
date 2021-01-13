afdeea67da83eeb97cf416faf7dd50da60cfae31
Marmouz82 <gtn.massart@gmail.com>
2021-01-13 16:49:29 +0100

    display info in the page-rest-info

--- wp-content/themes/dev-rest-rasta/page-rest-info.php
+++ wp-content/themes/dev-rest-rasta/page-rest-info.php
@@ -2,4 +2,30 @@
 
 <?php get_header(); ?>
 
-<?php 
+<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
+    
+    	<h1><?php the_title(); ?></h1>
+
+        
+
+        
+    	<?= get_field('phone_number'); ?>
+        <br>
+        <?= get_field('street_and_number'); ?>
+        <br>
+        <?= get_field('postcode_and_city'); ?>
+        <br>
+        <?= get_field('country'); ?>
+        <br>
+
+        <!-- recup sub_field of Opening -->
+
+        <?php if( have_rows('opening') ) :  while( have_rows('opening') ) : the_row(); ?>
+
+            <?= the_sub_field('opening_days'); ?>
+            <?= the_sub_field('opening_hours'); ?> <br>
+
+        <?php endwhile; endif; ?>
+
+
+<?php endwhile; endif; ?>
