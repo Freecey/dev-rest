738f7df436ab1132ab8660ba1f9e2fe67a9948f2
Marmouz82 <gtn.massart@gmail.com>
2021-01-14 14:36:13 +0100

    add bootstrap grid

--- wp-content/themes/dev-rest-rasta/page-rest-info.php
+++ wp-content/themes/dev-rest-rasta/page-rest-info.php
@@ -1,40 +1,55 @@
+<?php 
+/*
+Template Name: Informations Restaurant
+*/
+?>
+<?php get_header() ?>
 <!-- page-rest-info -->
 
-<?php get_header(); ?>
+<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
+    
+    <div class="container-fluid" style="background-color: grey;">
+        <div class="row">
+            <div class="col-3">
+                <h1><?php the_title(); ?></h1>
 
-        <?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
-            
-            <h1><?php the_title(); ?></h1>
+                <!-- Recup brieve_description from Restaurant description -->
+                <div class="rest_description">
+                    <?= get_field('brieve_description'); ?>
+                    <br>
+                </div>
+                <!-- Networks icons -->
+                
 
-            <!-- Recup brieve_description from Restaurant description -->
-            <div class="rest_description">
-                <?= get_field('brieve_description'); ?>
-                <br>
             </div>
 
             <!-- Recup fields from Contact us -->
-            <div class="contact_us">
-                <?= get_field('phone_number'); ?>
-                <br>
-                <?= get_field('street_and_number'); ?>
-                <br>
-                <?= get_field('postcode_and_city'); ?>
-                <br>
-                <?= get_field('country'); ?>
-                <br>
+            <div class="col-3">
+                <div class="contact_us">
+                    <?= get_field('phone_number'); ?>
+                    <br>
+                    <?= get_field('street_and_number'); ?>
+                    <br>
+                    <?= get_field('postcode_and_city'); ?>
+                    <br>
+                    <?= get_field('country'); ?>
+                    <br>
+                </div>
             </div>
 
             <!-- recup sub_field from Opening -->
+            <div class="col-3">
+                <!-- <?= the_title('opening'); ?> -->
+                <?php if( have_rows('opening') ) :  while( have_rows('opening') ) : the_row(); ?>
+                    <div class="div">
+                        <div class="opening">
+                            <?= the_sub_field('opening_days'); ?>
+                            <?= the_sub_field('opening_hours'); ?> <br>
+                        </div> 
+                    </div>   
+                <?php endwhile; endif; ?>
+            </div>
 
-            <!-- <?= the_title('opening'); ?> -->
-            <?php if( have_rows('opening') ) :  while( have_rows('opening') ) : the_row(); ?>
-
-            <div class="opening">
-                <?= the_sub_field('opening_days'); ?>
-                <?= the_sub_field('opening_hours'); ?> <br>
-            </div>    
-
-            <?php endwhile; endif; ?>
 
             <!-- Recup images from Instagram -->
 
@@ -44,18 +59,28 @@
 
             if( $images ): ?>
                 
-                    <?php foreach( $images as $image ): ?>
-
-                        <div class="instagram_images">
-                            <a href="<?= $image['url']; ?>">
-                                <img src="<?= $image['sizes']['thumbnail']; ?>" alt="<?= $image['alt']; ?>" />
-                            </a>
+                <div class="col-3">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-3">
+                                <?php foreach( $images as $image ): ?>
+                                                            
+                                    <a href="<?= $image['url']; ?>">
+                                        <img src="<?= $image['sizes']['thumbnail']; ?>" alt="<?= $image['alt']; ?>" />
+                                    </a>
+                                                                                                
+                                <?php endforeach; ?>
+                            </div>
                         </div>
-
-                    <?php endforeach; ?>
-                
+                    </div>                        
+                </div>
+                                   
             <?php endif; ?>
 
-                
+        </div>
+    </div>   
 
-        <?php endwhile; endif; ?>
+<?php endwhile; endif; ?>
+
+
+
