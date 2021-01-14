8b2d7af63c4d2aff5c880a9f840e4d576a06571c
Marmouz82 <gtn.massart@gmail.com>
2021-01-14 10:04:02 +0100

    dysplay data in page-rest-info.php

--- wp-content/themes/dev-rest-rasta/page-rest-info.php
+++ wp-content/themes/dev-rest-rasta/page-rest-info.php
@@ -2,34 +2,60 @@
 
 <?php get_header(); ?>
 
-<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
-    
-    	<h1><?php the_title(); ?></h1>
+        <?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
+            
+            <h1><?php the_title(); ?></h1>
 
+            <!-- Recup brieve_description from Restaurant description -->
+            <div class="rest_description">
+                <?= get_field('brieve_description'); ?>
+                <br>
+            </div>
 
-        <?= get_field('brieve_description'); ?>
-        <br>
-    	<?= get_field('phone_number'); ?>
-        <br>
-        <?= get_field('street_and_number'); ?>
-        <br>
-        <?= get_field('postcode_and_city'); ?>
-        <br>
-        <?= get_field('country'); ?>
-        <br>
+            <!-- Recup fields from Contact us -->
+            <div class="contact_us">
+                <?= get_field('phone_number'); ?>
+                <br>
+                <?= get_field('street_and_number'); ?>
+                <br>
+                <?= get_field('postcode_and_city'); ?>
+                <br>
+                <?= get_field('country'); ?>
+                <br>
+            </div>
 
-        <!-- recup sub_field of Opening -->
+            <!-- recup sub_field from Opening -->
 
-        <?php if( have_rows('opening') ) :  while( have_rows('opening') ) : the_row(); ?>
+            <!-- <?= the_title('opening'); ?> -->
+            <?php if( have_rows('opening') ) :  while( have_rows('opening') ) : the_row(); ?>
 
-            <?= the_sub_field('opening_days'); ?>
-            <?= the_sub_field('opening_hours'); ?> <br>
+            <div class="opening">
+                <?= the_sub_field('opening_days'); ?>
+                <?= the_sub_field('opening_hours'); ?> <br>
+            </div>    
+
+            <?php endwhile; endif; ?>
+
+            <!-- Recup images from Instagram -->
+
+            <?php 
+
+            $images = get_field('instagram');
+
+            if( $images ): ?>
+                
+                    <?php foreach( $images as $image ): ?>
+
+                        <div class="instagram_images">
+                            <a href="<?= $image['url']; ?>">
+                                <img src="<?= $image['sizes']['thumbnail']; ?>" alt="<?= $image['alt']; ?>" />
+                            </a>
+                        </div>
+
+                    <?php endforeach; ?>
+                
+            <?php endif; ?>
+
+                
 
         <?php endwhile; endif; ?>
-
-        <!-- Recup images Instagram -->
-
-        
-
-
-<?php endwhile; endif; ?>
