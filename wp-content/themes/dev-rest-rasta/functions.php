ceb28de01e6e8f6d8dbc11ef4a496defbe850809
KevKsar <cassart.kev@gmail.com>
2021-01-20 14:50:58 +0100

    add branch feature_nav_taxonomy_mobil

--- wp-content/themes/dev-rest-rasta/functions.php
+++ wp-content/themes/dev-rest-rasta/functions.php
@@ -135,7 +135,6 @@ add_action('admin_menu', 'add_links_themenu');
 add_action('acf / register_fields', 'my_register_fields');
 add_filter('acf/fields/google_map/api', 'my_acf_google_map_api');
 add_action('acf/init', 'my_acf_init');
-add_filter( 'excerpt_more', 'custom_excerpt_more' );
 
 
 
