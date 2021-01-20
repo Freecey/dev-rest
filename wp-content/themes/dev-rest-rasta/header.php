d5d24293b0ccd8a3fabd09511a2454e81a9de35d
Marmouz82 <gtn.massart@gmail.com>
2021-01-20 11:49:10 +0100

    try to fix issue in menu

--- wp-content/themes/dev-rest-rasta/header.php
+++ wp-content/themes/dev-rest-rasta/header.php
@@ -4,21 +4,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php wp_head(); ?> 
+    <?php wp_head([
+		'theme_location' => 'TOP NAVBAR']
+	); ?> 
 </head>
 <body>
 
 
-	<nav class="navigaton">
-		<div class="container navigation-box">
 
+
+	<nav class="navigaton">
+		<div class="container navigation-box" >
 			<h3 class="nav-restaurant-name">Dev Restaurant</h3>
 			<div class="ecartement"></div>
+
+			<?php wp_nav_menu(); ?>
+
 			<ul class="liste-nav">
 				<li class="item-nav"><a href="#Home"><img src="/wp-content/themes/dev-rest-rasta/assets/svg/home-minimal.svg" alt="" style="width: 10px;"></a></li>
 				<li class="item-nav"><a href="#Ours Restaurants">Ours Restaurants</a></li>
 				<li class="item-nav"><a href="#Menu">Menu</a></li>
-				<li class="item-nav"><a href="#Recipes">Recipes</a></li>
+				<li class="item-nav"><a href="#">Recipes</a></li>
 			</ul>
 			
 
