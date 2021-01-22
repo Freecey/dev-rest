40b31452aa96f6bd0d0a7ae41434f2d058d198a4
KevKsar <cassart.kev@gmail.com>
2021-01-22 19:03:19 +0100

    404 v

--- wp-content/themes/dev-rest-rasta/404.php
+++ wp-content/themes/dev-rest-rasta/404.php
@@ -1,3 +1,22 @@
-<!-- 404.php -->
+<?php get_header(); ?>
+<div class="topBanner"></div>
+<section class="section404">
+    <div class="container404">
+        <div class="section404__text">
+            <span id="e">E</span>
+            <span>R</span>
+            <span>R</span>
+            <span>O</span>
+            <span>R</span>
+        </div>
+        <div class="section404__404container">
+            <div class="section404__number" id="numL"></div>
+            <div id="burger" class="section404__burger">
+                <div class="section404__linkContainer"><a href="<?php echo home_url(); ?>" class="section404__link">Go Home</a></div>
+            </div>
+            <div class="section404__number" id="numR"></div>
+        </div>
+    </div>
+</section>
 
-<h1>Page not fund</h1>
+<?php get_footer(); ?>
